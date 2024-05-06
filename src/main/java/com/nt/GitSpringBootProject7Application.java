2ac6fb8a35ab9af7e8d2c5fd70a8e5c8313d05d8
deepa <deepanjalisahu535@gmail.com>
2024-05-06 15:54:46 +0800

    initial commit

---
+++ src/main/java/com/nt/GitSpringBootProject7Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringBootProject7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringBootProject7Application.class, args);
+	}
+
+}
